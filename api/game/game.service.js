8231a68402a93ce23592daf29ebb7e12d1f0d4aa
Alina <alina.boshkov@gmail.com>
2020-01-19 09:28:26 +0200

    small updates

--- api/game/game.service.js
+++ api/game/game.service.js
@@ -15,6 +15,7 @@ async function query(filterBy = {}) {
     const collection = await dbService.getCollection('game')
     try {
         let games = await collection.find(criteria).toArray();
+        console.log(games)
         // if (!games.length) {show message to the user/guest }
         return games
     } catch (err) {
@@ -71,8 +72,14 @@ async function add(game) {
 function _buildCriteria(filterBy) {
     const criteria = {};
     if (filterBy.publisherName) {
-        // criteria.publisher.user.userName = filterBy.publisherName
+        criteria.publisher = {
+            "user": {
+                userName:
+                    filterBy.publisherName
+            }
+        }
     }
+
     if (filterBy.minBalance) {
         criteria.balance = { $gte: +filterBy.minBalance }
     }
