816275491e928cc128820ef2b4d2d934afa9d88d
Daniil Glezer <daniilglezer@Daniils-MacBook-Pro.local>
2020-01-21 14:59:07 +0200

    added socket to play

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -4,7 +4,6 @@ module.exports = connectSockets
 function connectSockets(io) {
     io.on('connection', socket => {
         socket.on('chat newMsg', msg=>{
-            console.log(msg)
             // io.emit('chat addMsg', msg)
             // emits only to sockets in the same room
             io.to(socket.myTopic).emit('chat addMsg', msg)
@@ -16,5 +15,8 @@ function connectSockets(io) {
             socket.join(topic)
             socket.myTopic = topic;
         })
+        socket.on('user joined the game',userMessage=>{
+            io.to(socket.myTopic).emit('user joined the game',userMessage)
+         })
     })
 }
